4044353cfec2d3295c4e3537d00c75606183e756
jaywiles <jay.wiles@gmail.com>
2019-12-10 20:57:51 -0600

    classwork

---
+++ src/Router.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Switch, Route, Redirect } from 'react-router'
+import cookie from 'cookie'
+import Listings from './components/Listings'
+import Listing from './components/Listing'
+import Login from './components/Login'
+import Add from './components/Add'
+
+const checkAuth = () => {
+  const cookies = cookie.parse(document.cookie)
+  return cookies["loggedIn"] ? true : false
+}
+
+const ProtectedRoute = ({component: Component, ...rest}) => {
+  return (
+      <Route
+      {...rest}
+      render={(props) => checkAuth()
+          ? <Component {...props} />
+          : <Redirect to="/login" />}
+      />
+  )
+}
+
+const Router = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={Listings}/>
+      {/* how to target each individual business???? */}
+      <Route path="/listing/:id" component={Listing}/>
+      <Route path="login" component={Login}/>
+      <ProtectedRoute path="/add" component={Add}/>
+    </Switch>
+  );
+};
+
+export default Router;
